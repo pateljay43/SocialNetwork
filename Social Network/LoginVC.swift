1a51d3efd026d6519b16efb0f80f326303e99a29
pateljay143 <pateljay143@gmail.com>
2017-06-01 01:25:26 -0700

    Firebase setup completed

---
+++ Social Network/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Network
+//
+//  Created by JAY PATEL on 6/1/17.
+//  Copyright © 2017 Jay. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
