692a822d4ca342aa6376025e6c877f9a46782e70
Jay Patel <pateljay143@gmail.com>
2017-06-01 19:28:09 -0700

    Added custom UI effects to Login Screen

---
+++ Social Network/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social Network
+//
+//  Created by JAY PATEL on 6/1/17.
+//  Copyright © 2017 Jay. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 0.62)
